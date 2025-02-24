fe4f691117768f4864b9687cc4cff50066f75295
ac122381 <ac122381@8CG8163KX4>
2025-02-25 10:46:42 +1300

    Add project files.

---
+++ 13TPI Pet Adoption and Foster Centre NZ/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 13TPI Pet Adoption and Foster Centre NZ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
