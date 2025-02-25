a7743855051d290218c584e401d530cde4910cc2
Atul Herle <ac122381@avcol.school.nz>
2025-02-26 10:10:16 +1300

    Added fields to franchise model

--- 13TPI Pet Adoption and Foster Centre NZ/Models/Franchise.cs
+++ 13TPI Pet Adoption and Foster Centre NZ/Models/Franchise.cs
@@ -2,5 +2,15 @@
 {
     public class Franchise
     {
+        public int FranchiseID { get; set; }
+        public string FranchiseName { get; set; }
+
+        public string ContactNo { get; set; }
+
+        public int LocationID { get; set; }
+        public string EmailAddress { get; set; }
+        public string OperatingHours { get; set; }
+        public int OwnerID { get; set; }
+
     }
 }
