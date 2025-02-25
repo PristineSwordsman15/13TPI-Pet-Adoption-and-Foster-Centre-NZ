859eb1e1d73f2d2ae18bb84b889c47f6f12815e8
Atul Herle <ac122381@avcol.school.nz>
2025-02-26 09:51:14 +1300

    Created rest of my models AdminOffice Coordinator Franchises MedicalRecords Payments PetGroup Role Shelter User

---
+++ 13TPI Pet Adoption and Foster Centre NZ/Models/Payment.cs
@@ -0,0 +1,7 @@
+﻿
+namespace _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Models
+{
+    public class Payment
+    {
+    }
+}
---
+++ 13TPI Pet Adoption and Foster Centre NZ/Models/User.cs
@@ -0,0 +1,6 @@
+﻿namespace _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Models
+{
+    public class User
+    {
+    }
+}
