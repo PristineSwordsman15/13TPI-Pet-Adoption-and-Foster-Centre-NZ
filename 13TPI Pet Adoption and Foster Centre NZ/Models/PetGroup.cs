6fcd5a1f483f1bca166902d39694ccebf45c3e3e
Atul Herle <ac122381@avcol.school.nz>
2025-04-05 17:15:58 +1300

    Code comments of PetGroup

--- 13TPI Pet Adoption and Foster Centre NZ/Models/PetGroup.cs
+++ 13TPI Pet Adoption and Foster Centre NZ/Models/PetGroup.cs
@@ -6,9 +6,13 @@ namespace _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Models
 {
     public class PetGroup
     {
-        public int PetGroupID { get; set; }
+        // ID of PetGroup, pet belongs to
+        public int PetGroupID { get; set; } 
+        //ID of pet
         public int PetID { get; set;}
+        // Name of PetGroup
         public string PetGroupName { get; set; }
+        // Description of PetGroup
         public string PeGroupDescriptiom { get; set; }
     }
 }
