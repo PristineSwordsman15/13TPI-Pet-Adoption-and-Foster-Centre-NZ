bd465ca4392d38c473bd5b652442b325ddc5eade
Atul Herle <ac122381@avcol.school.nz>
2025-04-09 20:52:28 +1200

    fixed invalid name MedicalRecordID to add record

--- 13TPI Pet Adoption and Foster Centre NZ/Migrations/20250409084847_MedicalRecordID spelling adjusted.Designer.cs
+++ 13TPI Pet Adoption and Foster Centre NZ/Migrations/20250409084847_MedicalRecordID spelling adjusted.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data;
 
@@ -11,9 +12,11 @@ using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data;
 namespace _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250409084847_MedicalRecordID spelling adjusted")]
+    partial class MedicalRecordIDspellingadjusted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
