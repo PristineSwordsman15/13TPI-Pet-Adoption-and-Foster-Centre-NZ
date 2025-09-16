46f48b6cec9987064a2b44e46d3f610dbabbb507
Atul Herle <ac122381@avcol.school.nz>
2025-09-16 15:51:29 +1200

    Added indentity back into program cs

--- 13TPI Pet Adoption and Foster Centre NZ/Program.cs
+++ 13TPI Pet Adoption and Foster Centre NZ/Program.cs
@@ -1,6 +1,8 @@
+using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Areas.Identity.Data;
+using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data;
+using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data;
+using FluentAssertions.Common;
 using Microsoft.EntityFrameworkCore;
-using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data;
-using _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data;
 
 
 
@@ -10,8 +12,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ContextConnection")));
 
+// Use ApplicationUser
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<Context>();
+
 // MVC
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
