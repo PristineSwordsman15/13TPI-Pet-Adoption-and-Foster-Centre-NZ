e6407545dc41db831d561bd16904c8cd1c377cef
Atul Herle <ac122381@avcol.school.nz>
2025-10-18 13:23:02 +1300

    Added testiminials , featured pets, big buttons on the home page FOR accessiblity purpose . in the process of adding roles

--- 13TPI Pet Adoption and Foster Centre NZ/Program.cs
+++ 13TPI Pet Adoption and Foster Centre NZ/Program.cs
@@ -24,6 +24,21 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var config = services.GetRequiredService<IConfiguration>();
+    try
+    {
+        await _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data.RoleSeeder.SeedAsync(services, config);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Error while seeding roles.");
+    }
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -42,5 +57,7 @@ app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
