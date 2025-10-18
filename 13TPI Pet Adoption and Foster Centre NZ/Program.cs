0cc990cff3812cc5a9862f1245eb3c3859cc0ebf
Atul Herle <ac122381@avcol.school.nz>
2025-10-19 10:48:19 +1300

    Removed RolleSeeder as this a part of future developement

--- 13TPI Pet Adoption and Foster Centre NZ/Program.cs
+++ 13TPI Pet Adoption and Foster Centre NZ/Program.cs
@@ -24,21 +24,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var config = services.GetRequiredService<IConfiguration>();
-    try
-    {
-        await _13TPI_Pet_Adoption_and_Foster_Centre_NZ.Data.RoleSeeder.SeedAsync(services, config);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "Error while seeding roles.");
-    }
-}
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
